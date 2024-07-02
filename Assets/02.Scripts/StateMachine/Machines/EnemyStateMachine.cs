f5cdd6afa284897cc246e3afa5a58efaa8e32091
Chloe <144107013+Charen523@users.noreply.github.com>
2024-07-02 17:05:04 +0900

    [feat] EnemyStateMachine 임시 구현

--- Assets/02.Scripts/StateMachine/Machines/EnemyStateMachine.cs
+++ Assets/02.Scripts/StateMachine/Machines/EnemyStateMachine.cs
@@ -1,5 +1,30 @@
 
+using UnityEngine;
+
 public class EnemyStateMachine : DeathableStateMachine
 {
-    
+    public Enemy enemy;
+    public GameObject target; //플레이어.
+
+    #region Enemy States
+    public EnemyChaseState ChaseState {  get; private set; }
+    public EnemyAttackState AttackState { get; private set; }
+    public EnemyDieState DieState { get; private set; }
+    #endregion
+
+    /*movement status*/
+    public Vector2 moveInput;
+    public float moveSpeed;
+    public float movementSpeedModifier;
+    public float rotationDamping;
+
+    public EnemyStateMachine (Enemy enemy)
+    {
+        this.enemy = enemy;
+        //target 초기화.
+        
+        /*states 인스턴스 생성*/
+
+        /*movement status를 Enemy.Data로부터 받아오기*/
+    }
 }
