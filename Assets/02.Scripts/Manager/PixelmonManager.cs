cf9a24497f039f7e95f72e1a79c37fbf239f7abf
Euronia12 <lkh0647@naver.com>
2024-07-02 16:59:03 +0900

    [add]픽셀몬 매니저 등록

---
+++ Assets/02.Scripts/Manager/PixelmonManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelmonManager : Singleton<PixelmonManager>
+{
+    public int currentPixelmonCount;
+    public Pixelmon[] Pixelmons = new Pixelmon[5];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
