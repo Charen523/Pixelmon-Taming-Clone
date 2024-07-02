8f6faac03bb8c43c24a076e8c150433478c2e991
Euronia12 <lkh0647@naver.com>
2024-07-02 20:56:50 +0900

    [remove] 상속받은 내용 삭제

--- Assets/02.Scripts/StateMachine/States/Pixelmon/PixelmonAnimationData.cs
+++ Assets/02.Scripts/StateMachine/States/Pixelmon/PixelmonAnimationData.cs
@@ -5,24 +5,12 @@ using UnityEngine;
 public class PixelmonAnimationData
 {
     [SerializeField]
-    private string idleParameterName = "Idle";
-    [SerializeField]
-    private string walkParameterName = "Walk";
-    [SerializeField]
-    private string attackParameterName = "Attack";
-    [SerializeField]
     private string skillParameterName = "Skill";
 
-    public int IdleParameterHash {  get; private set; }
-    public int WalkParameterHash { get; private set;}
-    public int AttackParameterHash { get; private set;}
     public int SkillParameterHash { get; private set;}
 
     public void Initialize()
     {
-        IdleParameterHash = Animator.StringToHash(idleParameterName);
-        WalkParameterHash = Animator.StringToHash(walkParameterName);
-        AttackParameterHash = Animator.StringToHash(attackParameterName);
         SkillParameterHash = Animator.StringToHash(skillParameterName);
     }
 }
