6a171c9b144f8336ab543f4e2f83d1890df91872
Chloe <144107013+Charen523@users.noreply.github.com>
2024-07-02 17:05:42 +0900

    [feat] EnemyChaseState 임시 구현

---
+++ Assets/02.Scripts/StateMachine/States/Enemy/EnemyChaseState.cs
@@ -0,0 +1,45 @@
+﻿
+public class EnemyChaseState : EnemyBaseState
+{
+    public EnemyChaseState(EnemyStateMachine stateMachine) : base(stateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        stateMachine.movementSpeedModifier = 1;
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (IsDead())
+        {
+            stateMachine.ChangeState(stateMachine.DieState);
+        }
+        else if (IsAttackRange())
+        {
+            stateMachine.ChangeState(stateMachine.AttackState);
+        }
+    }
+
+    protected bool IsDead()
+    {
+        //애니메이션 GetBool로 죽음 상태 가져오기??
+        return false;
+    }
+
+    protected bool IsAttackRange()
+    {
+        float playerDistanceSqr = (stateMachine.target.transform.position - stateMachine.enemy.transform.position).sqrMagnitude;
+        return playerDistanceSqr <= stateMachine.enemy.attackDistance * stateMachine.enemy.attackDistance;
+    }
+}
+}
