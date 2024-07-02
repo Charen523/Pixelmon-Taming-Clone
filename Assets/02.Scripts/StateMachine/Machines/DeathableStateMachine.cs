cdb1f1d3acd6eab8b305c9307af3b8d53cd8d0f1
SungJiYoon <66246253+ss-zun@users.noreply.github.com>
2024-07-03 02:51:04 +0900

    [feat] DeathableStateMachine (#16)
    적이 죽을 때 DieState가 달라져서 수정필요

--- Assets/02.Scripts/StateMachine/Machines/DeathableStateMachine.cs
+++ Assets/02.Scripts/StateMachine/Machines/DeathableStateMachine.cs
@@ -1,5 +1,15 @@
 
+using UnityEngine;
+
 public class DeathableStateMachine : BaseStateMachine
 {
+    [HideInInspector] public DieState dieState;
+    [HideInInspector] public HitState hitState;
 
+    protected override void Start()
+    {
+        base.Start();
+        dieState = new DieState(this);
+        hitState = new HitState(this);
+    }
 }
