600526831e01f5eb4170f0515938c8d64f6bd659
Chloe <144107013+Charen523@users.noreply.github.com>
2024-07-05 21:06:58 +0900

    [feat] enemystate 변경된 부모에 맞게 수정

--- Assets/02.Scripts/StateMachine/Machines/EnemyStateMachine.cs
+++ Assets/02.Scripts/StateMachine/Machines/EnemyStateMachine.cs
@@ -6,18 +6,30 @@ public class EnemyStateMachine : StateMachine
     public GameObject target; //어케 찾아올까유~ 플레이어 싱글톤?
     
     #region Enemy States
+    public EnemyIdleState IdleState { get; private set; }
     public EnemyChaseState ChaseState {  get; private set; }
     public EnemyAttackState AttackState { get; private set; }
     public EnemyDieState DieState { get; private set; }
     #endregion
 
+    private void OnEnable()
+    {
+        ChangeState(ChaseState);
+    }
+
     private void Start()
     {
+        target = Player.Instance.gameObject;
+
         MovementSpeed = data.spd;
         AttackRange = data.atkRange;
 
-        ChaseState = new EnemyChaseState(this);
+        ChaseState = new EnemyChaseState(this, target.transform);
         AttackState = new EnemyAttackState(this);
         DieState = new EnemyDieState(this);
+
+        GameManager.Instance.OnPlayerDie += () => ChangeState(IdleState);
+        GameManager.Instance.OnStageTimeOut += () => ChangeState(IdleState);
+        ChaseState.OnTargetReached += () => ChangeState(AttackState);
     }
 }
