3eb9676ffed5ab8d06af00801a9bef4bd7646b25
Chloe <144107013+Charen523@users.noreply.github.com>
2024-07-05 16:35:00 +0900

    [move] 스크립트 위치 정리

---
+++ Assets/02.Scripts/Manager/PoolManager.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Pool
+{
+    public string tag;
+    public GameObject prefab;
+    public int size;
+}
+
+public class PoolManager : Singleton<PoolManager>
+{
+    public List<Pool> Pools;
+    public Dictionary<string, Queue<GameObject>> PoolDictionary;
+
+    protected override void Awake()
+    {
+        PoolDictionary = new Dictionary<string, Queue<GameObject>>();
+        foreach (var pool in Pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+            PoolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+    public GameObject SpawnFromPool(string tag)
+    {
+        if (!PoolDictionary.ContainsKey(tag))
+            return null;
+
+        GameObject obj = PoolDictionary[tag].Dequeue();
+        PoolDictionary[tag].Enqueue(obj);
+        obj.SetActive(true);
+        return obj;
+    }
+
+    public GameObject[] GetActiveObjectsFromPool(string tag)
+    {
+        if (!PoolDictionary.ContainsKey(tag))
+            return new GameObject[0];
+
+        List<GameObject> activeObjects = new List<GameObject>();
+        foreach (var obj in PoolDictionary[tag])
+        {
+            if (obj.activeInHierarchy)
+            {
+                activeObjects.Add(obj);
+            }
+        }
+        return activeObjects.ToArray();
+    }
+}
