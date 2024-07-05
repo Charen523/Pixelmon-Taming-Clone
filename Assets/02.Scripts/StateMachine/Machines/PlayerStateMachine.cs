baed90fe4c39c2a9f212833930cf8aa210384f05
SungJiYoon <66246253+ss-zun@users.noreply.github.com>
2024-07-05 10:40:19 +0900

    Feat/player (#32)
    * [update] Player.prefab
    
    * [update] 코드 컨벤션

--- Assets/02.Scripts/StateMachine/Machines/PlayerStateMachine.cs
+++ Assets/02.Scripts/StateMachine/Machines/PlayerStateMachine.cs
@@ -8,29 +8,29 @@ public class PlayerStateMachine : StateMachine
     public Vector2 MovementInput { get; set; }
 
     // States
-    public IdleState idleState { get; private set; }
-    public PlayerDetectState detectState { get; private set; }
-    public PlayerMoveState moveState { get; private set; }
+    public IdleState IdleState { get; private set; }
+    public PlayerDetectState DetectState { get; private set; }
+    public PlayerMoveState MoveState { get; private set; }
 
     private void Start()
     {
         MovementSpeed = 3f;
         AttackRange = 2f;
 
-        idleState = new IdleState(this);
-        detectState = new PlayerDetectState(this);
-        moveState = new PlayerMoveState(this, null);
+        IdleState = new IdleState(this);
+        DetectState = new PlayerDetectState(this);
+        MoveState = new PlayerMoveState(this, null);
 
-        GameManager.Instance.OnGameStarted += () => ChangeState(detectState);   
-        GameManager.Instance.OnGameEnded += () => ChangeState(idleState);
-        moveState.OnTargetReached += ChangeAttackState;
+        GameManager.Instance.OnGameStarted += () => ChangeState(DetectState);   
+        GameManager.Instance.OnGameEnded += () => ChangeState(IdleState);
+        MoveState.OnTargetReached += ChangeAttackState;
 
-        ChangeState(detectState);
+        ChangeState(DetectState);
     }
 
     // 공격 상태로 변경(플레이어는 idle 상태)
     private void ChangeAttackState()
     {
-        ChangeState(idleState);
+        ChangeState(IdleState);
     }
 }
