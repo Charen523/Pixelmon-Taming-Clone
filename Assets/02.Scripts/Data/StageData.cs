a240b327fc980a9296874b27dce8a529b98e9606
Euronia12 <lkh0647@naver.com>
2024-07-05 21:23:10 +0900

    [update]문자열 리스트로 변수 변경

--- Assets/02.Scripts/Data/StageData.cs
+++ Assets/02.Scripts/Data/StageData.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 [Serializable]
 public class StageData : IData
@@ -10,12 +11,12 @@ public class StageData : IData
     public int stageId;
     public int spawnCount;
     public int nextStageCount;
-    public string monsterIds;
+    public List<string> monsterIds;
     public string bossId;
-    public string rewardTypes;
-    public string rewardValues;
-    public string offlineRewardTypes;
-    public string offlineRewardValues;
+    public List<string> rewardTypes;
+    public List<string> rewardValues;
+    public List<string> offlineRewardTypes;
+    public List<string> offlineRewardValues;
 
     string IData.Rcode => rcode;  // 명시적 인터페이스 구현
 }
