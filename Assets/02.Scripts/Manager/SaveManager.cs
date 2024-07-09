5a9b2e69424e51cde64105d2a439659faaae8d66
Euronia12 <lkh0647@naver.com>
2024-07-09 12:05:29 +0900

    [feat]json 세이브 기능 추가

--- Assets/02.Scripts/Manager/SaveManager.cs
+++ Assets/02.Scripts/Manager/SaveManager.cs
@@ -1,15 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class SaveManager : Singleton<DataManager>
 {
     private readonly string jsonPlayerData = "PlayerData";
-
+    private readonly string jsonFolder = Application.persistentDataPath;
     [SerializeField]
     Dictionary<string, PlayerData> PlayerDictionary = new Dictionary<string, PlayerData>();
+
+    [SerializeField]
+    private string playerPrefs;
+    private PlayerData playerData;
     void Start()
     {
-        
+     playerData = Player.Instance   
     }
+
+    public void SaveUserData()
+    {
+
+    }
+
+    private void SaveToJsonData<T>(T data, string path)
+    {
+        string fileName = Path.Combine(jsonFolder, path);
+        if(!File.Exists(fileName)) 
+        {
+            File.Create(fileName);
+        }
+        string dataJson = JsonUtility.ToJson(data);
+        File.WriteAllText(fileName, dataJson);
+    }    
 }
