c5bb10145f0f258de8f3535bd6d441d72107401a
Euronia12 <lkh0647@naver.com>
2024-07-10 15:40:34 +0900

    [feat] 더티플래그패턴 기초작업

--- Assets/02.Scripts/Manager/InventoryManager.cs
+++ Assets/02.Scripts/Manager/InventoryManager.cs
@@ -1,23 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class InventoryManager : Singleton<InventoryManager>
 {
-    private UserData userData;
-    public UserData UserData { 
-        get { return userData; }
-        set { }
-    }
+    [SerializeField]
+    private UserData userData = new UserData();
+
+    bool isDirty;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(ChagnedValue());
+        SetData(nameof(userData.money), 10);
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ChagnedValue()
     {
-        
+        while (true)
+        {
+            if (isDirty)
+            {
+                isDirty = false;
+                SaveManager.Instance.SaveToPrefs(userData);
+            }
+            yield return null;
+        }
+    }
+
+    public void SetData(string field, object value)
+    {
+        var fields = userData.GetType().GetField(field);
+        fields.SetValue(userData, value);
+        isDirty = true;
+    }
+
+
+    private void DropItem(string rcode, int count)
+    {
+        SetData(DataManager.Instance.GetData<RewardData>(rcode).name, count);
     }
 }
+
