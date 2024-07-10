7f7994ed6058967eb70583994a04eb517a2d1bce
Euronia12 <lkh0647@naver.com>
2024-07-10 11:11:47 +0900

    [feat] 인벤토리매니저 추가

---
+++ Assets/02.Scripts/Manager/InventoryManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : Singleton<InventoryManager>
+{
+    private UserData userData;
+    public UserData UserData { 
+        get { return userData; }
+        set { }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
