e012f89a04159497d6f0a0c60383ed08f2743789
Euronia12 <lkh0647@naver.com>
2024-07-12 19:53:37 +0900

    [update]픽셀몬데이터 수정
    도감넘버 변수
    중복개수 변수
    편성여부 변수
    보유여부 변수
    스킬 셋 추가

--- Assets/02.Scripts/Data/PixelmonData.cs
+++ Assets/02.Scripts/Data/PixelmonData.cs
@@ -5,15 +5,25 @@ using UnityEngine.Events;
 public class PixelmonData : IData
 {
     public string rcode;
+    //도감 넘버
+    public int collectionNum;
     public string name;
     public int rank;
+    //같은 카드 중복개수
+    public int possessionCount;
     public float atk;
     public float sAtk;
     public float cri;
     public float criDmg;
     public float atkDmg;
     public float hit;
+    //편성 여부
+    public bool isComposed;
+    //보유 여부
+    public bool isPossess;
 
     public UnityAction activeSkill;
+    public UnityAction passiveSkill;
+    public UnityAction trait;
     string IData.Rcode => rcode;  // 명시적 인터페이스 구현
 }
