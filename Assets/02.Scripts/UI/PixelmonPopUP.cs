14704c6142fb0bd110058fea156a86885d17afcd
Euronia12 <lkh0647@naver.com>
2024-07-14 17:22:14 +0900

    [feat]픽셀몬팝업 스크립트 생성

---
+++ Assets/02.Scripts/UI/PixelmonPopUP.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelmonPopUP : MonoBehaviour
+{
+    [SerializeField]
+    private PixelmonTab tab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void InitPixelmonData(int dataIndex)
+    {
+        
+    }
+
+    public void PreviousInfo()
+    {
+
+    }
+    public void NextInfo()
+    {
+
+    }
+
+}
