c13cd28a9d39cc8e9ff04f30c45b6812d9644cd5
Euronia12 <lkh0647@naver.com>
2024-07-15 10:10:03 +0900

    [feat]슬롯 정보 초기화

--- Assets/02.Scripts/UI/PixelmonSlot.cs
+++ Assets/02.Scripts/UI/PixelmonSlot.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PixelmonSlot : MonoBehaviour
 {
     public PixelmonData pixelmonData;
     public GameObject lockIcon;
+    [SerializeField]
+    private Image SlotIcon;
     // Start is called before the first frame update
-    void Start()
+
+    public void InitSlot(PixelmonData data)
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RemoveInfo()
     {
-        
+        pixelmonData = null;
+        SlotIcon.sprite = null;
     }
 }
