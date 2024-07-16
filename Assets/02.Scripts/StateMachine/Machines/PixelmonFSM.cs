634f5909b049650008855351381f9c9f502c5781
Euronia12 <lkh0647@naver.com>
2024-07-16 13:02:43 +0900

    [feat]픽셀몬 적 분할 공격 구현
    각 픽셀몬이 개별 적을 공격할 수 있도록 구현

--- Assets/02.Scripts/StateMachine/Machines/PixelmonFSM.cs
+++ Assets/02.Scripts/StateMachine/Machines/PixelmonFSM.cs
@@ -1,11 +1,16 @@
 
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
 
 public class PixelmonFSM : FSM
 {
     public Pixelmon pixelmon;
+    public string EnemyTag = "Enemy";
 
     #region Pixelmon States
     public IdleState IdleState { get; private set; }
@@ -17,6 +22,7 @@ public class PixelmonFSM : FSM
     float attackSpeed = 1;
     WaitUntil waitAttack;
     float coolTime = 0;
+    float minDistance;
     private void Start()
     {
         waitAttack = new WaitUntil(() => AttackCoolTime());
@@ -28,7 +34,7 @@ public class PixelmonFSM : FSM
         IdleState = new IdleState(this);
         MoveState = new PixelmonMoveState(this);
         AttackState = new PixelmonAttackState(this);
-
+        minDistance = Player.Instance.data.atkRange;
         ChangeState(IdleState);
     }
 
@@ -38,20 +44,18 @@ public class PixelmonFSM : FSM
         {
             //몹이 null이 아니라면 공격 및 스킬
             //기다리는 시간과 날아가는 시간에 대한 보정 필요!!!!!
-            if (target != null)
+            if (coolTime == 0)
             {
-                if (coolTime == 0)
+                var enemies = Search(1);
+                for (int i = 0; i < enemies.Count; i++)
                 {
-                    Vector2 direction = target.transform.position - transform.position;
+                    Vector2 direction = enemies[i].transform.position - transform.position;
                     float damage = pixelmon.data.atkDmg;
-                    float bulletRange = 2f; //임의의 변수
-
                     GameObject projectile = PoolManager.Instance.SpawnFromPool("ATV00000");
-                    projectile.GetComponent<ProjectileController>().GetAttackSign(transform.position, direction, damage, bulletRange, 1);
-
+                    projectile.GetComponent<ProjectileController>().GetAttackSign(transform.position, direction, damage, minDistance, 5);
                 }
-                yield return waitAttack;
             }
+            yield return waitAttack;
         }
     }
 
@@ -80,5 +84,20 @@ public class PixelmonFSM : FSM
             StopCoroutine(attackCoroutine);
             attackCoroutine = null;
         }
+    }
+
+    public List<Collider2D> Search(int count)
+    {
+        var hitColliders = Physics2D.OverlapCircleAll(Player.Instance.transform.position, minDistance).ToList();
+        hitColliders.RemoveAll(obj => !obj.gameObject.CompareTag(EnemyTag));
+        hitColliders.Sort((a, b) =>
+        {
+            var aDist = (transform.position - a.gameObject.transform.position).sqrMagnitude;
+            var bDist = (transform.position - b.gameObject.transform.position).sqrMagnitude;
+            return aDist.CompareTo(bDist);
+        });
+        count = Mathf.Min(count, hitColliders.Count);
+        hitColliders.RemoveRange(count, hitColliders.Count - count);
+        return hitColliders;
     }
 }
