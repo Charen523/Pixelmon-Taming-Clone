5d0cae4613b3d06511f6a2e61b8e6e2fcea5cfa7
Euronia12 <lkh0647@naver.com>
2024-07-17 19:41:43 +0900

    [feat]픽셀몬장착슬롯 추가

---
+++ Assets/02.Scripts/UI/PixelmonEquipSlot.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class PixelmonEquipSlot : PixelmonSlot
+{
+    public GameObject lockIcon;
+    public bool isLocked = true;
+
+    private void Start()
+    {
+        slotBtn.onClick.AddListener(OnClick);
+        ChangedInfo();      
+    }
+
+    public void ChangedInfo()
+    {
+        PixelmonData[] datas = InventoryManager.Instance.userData.equipedPixelmons;
+        if (datas.Length > gameObject.transform.GetSiblingIndex())
+        {
+            pixelmonData = datas[gameObject.transform.GetSiblingIndex()];
+        }
+    }
+
+    public override void InitSlot(PixelmonTab tab, PixelmonData data)
+    {
+        base.InitSlot(tab, data);
+    }
+
+    public void Equip(PixelmonData data)
+    {
+        pixelmonData = data;
+        slotIcon.sprite = data.icon;
+        data.isEquiped = true;
+       
+    }
+
+    public void UnEquip()
+    {
+        pixelmonData = null;
+        slotIcon.sprite = null;
+    }
+}
