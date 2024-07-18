2d25dec67426b321c35e28b4c4a6181be8d9fcf2
Euronia12 <lkh0647@naver.com>
2024-07-18 14:45:07 +0900

    [feat]픽셀몬레이아웃 장착/해제용 액션 추가

--- Assets/02.Scripts/Manager/InventoryManager.cs
+++ Assets/02.Scripts/Manager/InventoryManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InventoryManager : Singleton<InventoryManager>
 {
@@ -11,6 +12,8 @@ public class InventoryManager : Singleton<InventoryManager>
     [SerializeField]
     private DataManager dataManager;
     bool isDirty;
+    public UnityAction<int, PixelmonData> equipAction;
+    public UnityAction<int> unEquipAction;
     // Start is called before the first frame update
     void Start()
     {
