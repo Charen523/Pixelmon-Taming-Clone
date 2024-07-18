30667bda16801017acb38bd38debfdf22d53c7fa
Euronia12 <lkh0647@naver.com>
2024-07-18 16:07:59 +0900

    [feat] 픽셀몬 컴포넌트 캐싱

--- Assets/02.Scripts/Entity/Pixelmon.cs
+++ Assets/02.Scripts/Entity/Pixelmon.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pixelmon : MonoBehaviour
 {
     public PixelmonData data;
     public PixelmonFSM fsm;
+    public SpriteRenderer spriteRenderer;
+    public Animator animator;
     private void Start()
     {
         fsm.InitStates();
