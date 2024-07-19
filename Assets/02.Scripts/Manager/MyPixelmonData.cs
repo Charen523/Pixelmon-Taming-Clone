d4b5890dfc2b90e1b38767c4465da378c94432ad
Euronia12 <lkh0647@naver.com>
2024-07-19 18:19:40 +0900

    [update]픽셀몬데이터 이동

---
+++ Assets/02.Scripts/Manager/MyPixelmonData.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MyPixelmonData
+{
+    //도감 넘버
+    public int id;
+    public int lv = 1;
+    public int exp = 0;
+    public int star;
+    //같은 카드 중복개수
+    public int evolvedCount;
+
+    //장착 여부
+    public bool isEquiped;
+    //보유 여부
+    public bool isPossessed;
+
+    public UnityAction activeSkill;
+    //패시브 능력
+    public PsvSkill[] trait;
+}
+
+public class PsvSkill
+{
+    public string rcode;
+    //패시브 능력 이름
+    public string name;
+    //픽셀몬 패시브 타입
+    public PassiveType psvType;
+    //패시브 능력치
+    public float ability;
+}
