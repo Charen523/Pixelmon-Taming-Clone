25b4603b8e09b24a67231fecb263555ef287ca40
Euronia12 <lkh0647@naver.com>
2024-07-19 14:57:45 +0900

    [feat]레벨 및 슬라이더바 적용

--- Assets/02.Scripts/UI/PixelmonSlot.cs
+++ Assets/02.Scripts/UI/PixelmonSlot.cs
@@ -1,30 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PixelmonSlot : MonoBehaviour
 {
-    public PixelmonData pixelmonData;
-    public PixelmonTab pixelmontab;
     [SerializeField]
     protected RectTransform rectTr;
-    public bool isPossessed => pixelmonData.isPossessed;
-    public int slotIndex;
     [SerializeField]
     protected Button slotBtn;
     [SerializeField]
     protected Image slotIcon;
+
+    public PixelmonData pixelmonData;
+    public PixelmonTab pixelmontab;
+    public TextMeshProUGUI propertyEffectTxt;
+    public bool isPossessed => pixelmonData.isPossessed;
+
+    public int slotIndex;
+    [SerializeField]
+    protected TextMeshProUGUI lvTxt;
+    [SerializeField]
+    private Slider evolveSldr;
+    [SerializeField]
+    private TextMeshProUGUI evolveTxt;
+    [SerializeField]
+    protected GameObject[] stars;
+
+    private DataManager dataManager;
     // Start is called before the first frame update
 
 
     public virtual void InitSlot(PixelmonTab tab, PixelmonData data)
     {
+        dataManager = DataManager.Instance;
         pixelmontab = tab;
         pixelmonData = data;
         slotIcon.sprite = pixelmonData.icon;
         slotBtn.onClick.AddListener(OnClick);
+        UpdateSlot();
         //if (isLocked) lockIcon.SetActive(false);
+    }
+
+    public virtual void UpdateSlot()
+    {
+        slotIcon.sprite = pixelmonData.icon;
+        lvTxt.text = string.Format("Lv.{0}", pixelmonData.lv);
+        SetStars();
+        SetEvolveSldr();
+    }
+
+    public void SetStars()
+    {
+        if (stars.Length > 0)
+        {
+            for (int i = 0; i < pixelmonData.star; i++)
+            {
+                stars[i].SetActive(true);
+            }
+        }
+    }
+
+    public void SetEvolveSldr()
+    {
+        int maxNum = GetEvolveValue();
+        evolveSldr.maxValue = maxNum;
+        evolveSldr.value = pixelmonData.currentCount;
+        evolveTxt.text = string.Format("{0}/{1}", pixelmonData.currentCount, maxNum);
+    }
+
+    public int GetEvolveValue()
+    {
+        switch (pixelmonData.star)
+        {
+            case 0:
+                return dataManager.GetData<EvolveData>(pixelmonData.rank).start1;
+            case 1:
+                return dataManager.GetData<EvolveData>(pixelmonData.rank).start2;
+            case 2:
+                return dataManager.GetData<EvolveData>(pixelmonData.rank).start3;
+            case 3:
+                return dataManager.GetData<EvolveData>(pixelmonData.rank).start4;
+            case 4:
+                return dataManager.GetData<EvolveData>(pixelmonData.rank).start5;
+            default:
+                return 0;
+        }
     }
 
     protected virtual void OnClick()
