e80070e21219e68f66cbb7e9289a46632d97f134
Chloe <144107013+Charen523@users.noreply.github.com>
2024-07-22 11:06:58 +0900

    [chore] 불필요 주석 삭제

--- Assets/02.Scripts/Data/UserData.cs
+++ Assets/02.Scripts/Data/UserData.cs
@@ -24,7 +24,7 @@ public class UserData
     //public string curQuestRode;
 
     //public int[] StatUpLvs = new int[10]; //나중에 개수 보고 바꾸기.
-    public MyPixelmonData[] equippedPxms = new MyPixelmonData[5]; //string으로 바꾸기.
+    public MyPixelmonData[] equippedPxms = new MyPixelmonData[5];
     public MyPixelmonData[] ownedPxms = new MyPixelmonData[20];
     //public string[] OwnedActiveSkills; 만약 스킬 강화 생기면 string-> 스킬로 바꾸기.
     public FieldData[] fieldDatas = new FieldData[6];
