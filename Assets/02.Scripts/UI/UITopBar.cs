78ddc6f266a009205239161fc3862ea1a331e878
Chloe <144107013+Charen523@users.noreply.github.com>
2024-07-24 12:34:56 +0900

    [feat] Top UI 골드 고치는 중

--- Assets/02.Scripts/UI/UITopBar.cs
+++ Assets/02.Scripts/UI/UITopBar.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 
 public class UITopBar : UIBase
 {
-    private SaveManager invenManager;
+    private SaveManager saveManager;
 
     #region User Info UI
     [SerializeField] private Image characImg;
@@ -19,29 +19,64 @@ public class UITopBar : UIBase
     #endregion
 
     #region Data Fields
-    private int currentExp; 
-    private readonly float tempMaxExp = 1000; //임시 변수
+    private int currentExp => saveManager.userData.userExp; 
+    private readonly int tempMaxExp = 1000; //임시 변수
     #endregion
 
     private void Awake()
     {
-        invenManager = SaveManager.Instance;
+        saveManager = SaveManager.Instance;
     }
 
     private void Start()
     {
         InitTopUIData();
+        saveManager.UpdateUI += UpdateTopUI;
     }
 
     private void InitTopUIData()
     {
-        lvTxt.text = $"Lv.{invenManager.userData.userLv}";
+        lvTxt.text = $"Lv.{saveManager.userData.userLv}";
         
-        userName.text = invenManager.userData.userName;
-        goldTxt.text = invenManager.userData.gold.ToString();
-        gemTxt.text = invenManager.userData.diamond.ToString();
+        userName.text = saveManager.userData.userName;
+        goldTxt.text = saveManager.userData.gold.ToString();
+        gemTxt.text = saveManager.userData.diamond.ToString();
 
-        currentExp = invenManager.userData.userExp;
+        expSldr.value = currentExp / tempMaxExp;
+        expTxt.text = (expSldr.value * 100).ToString("0.00") + "%";
+    }
+
+    private void UpdateTopUI(int index)
+    {
+        switch(index)
+        {
+            case 0:
+                UpdateRewardTxt();
+                break;
+            case 1:
+                UpdateExpTxt();
+                break;
+            default:
+                Debug.LogWarning("업데이트 인덱스 오류");
+                break;
+        }
+    }
+
+    public void UpdateRewardTxt()
+    {
+        goldTxt.text = saveManager.userData.gold.ToString();
+        gemTxt.text = saveManager.userData.diamond.ToString();
+    }
+
+    public void UpdateExpTxt()
+    {
+        while (currentExp > tempMaxExp)
+        {
+            saveManager.SetDeltaData("userExp", -tempMaxExp);
+            saveManager.userData.userLv++;
+            lvTxt.text = $"Lv.{saveManager.userData.userLv}";
+        }
+
         expSldr.value = currentExp / tempMaxExp;
         expTxt.text = (expSldr.value * 100).ToString("0.00") + "%";
     }
