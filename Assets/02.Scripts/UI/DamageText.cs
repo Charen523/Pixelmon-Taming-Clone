a13786b5bdead5b85cd636da920825e9ea33ae26
Euronia12 <lkh0647@naver.com>
2024-07-24 12:20:47 +0900

    [feat]피격시 데미지 표시 스크립트 추가

---
+++ Assets/02.Scripts/UI/DamageText.cs
@@ -0,0 +1,58 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+public class DamageText : SerializedMonoBehaviour
+{
+    [SerializeField]
+    Transform parent;
+    Coroutine OnDamage;
+    Camera cam;
+    [SerializeField]
+    TextMeshProUGUI damageTxt;
+    [SerializeField]
+    RectTransform rectTr;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    public void ShowDamageText(int damage, Vector3 pos)
+    {
+        if (OnDamage != null)
+            StopCoroutine(ShowText(pos));
+        damageTxt.text = damage.ToString();
+        OnDamage = StartCoroutine(ShowText(pos));
+    }
+
+    IEnumerator ShowText(Vector3 pos)
+    {
+        rectTr.anchoredPosition = RectTransformUtility.WorldToScreenPoint(cam, pos);
+        yield return null;
+        float time = 0;
+        Color textColor = Color.white;
+        damageTxt.color = Color.white;
+        while (time < 3f)
+        {
+            time += Time.deltaTime;
+            rectTr.anchoredPosition = RectTransformUtility.WorldToScreenPoint(cam, Vector3.Lerp(pos, pos + Vector3.up* 5, time / 3));
+            textColor.a = Mathf.Lerp(damageTxt.color.a, 0, time / 3);
+            damageTxt.color = textColor;
+            yield return null;
+        }
+        damageTxt.text = null;
+        gameObject.SetActive(false);
+    }
+
+
+    private void OnDisable()
+    {
+        if (OnDamage != null)
+            StopCoroutine(OnDamage);
+    }
+}
