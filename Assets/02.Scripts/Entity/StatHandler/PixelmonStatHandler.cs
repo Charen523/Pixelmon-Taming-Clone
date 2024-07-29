cdff23edb2700e37da6d2cc1d31e6677853e0597
Euronia12 <lkh0647@naver.com>
2024-07-29 17:23:44 +0900

    [update]스탯핸들러 임시 변경

--- Assets/02.Scripts/Entity/StatHandler/PixelmonStatHandler.cs
+++ Assets/02.Scripts/Entity/StatHandler/PixelmonStatHandler.cs
@@ -4,27 +4,36 @@ public static class PixelmonStatHandler
 {
     public static PixelmonStatus InitStatus(this PixelmonStatus status, PixelmonData data, MyPixelmonData myData)
     {
-        status.Atk = SetStatus(data.baseAtk, myData.statRank[(int)PxmStatus.Atk], myData.lv, 0, 10);
-        status.Cri = SetPercentStatus(data.baseAtk, myData.statRank[(int)PxmStatus.Cri], myData.lv, 0, 10);
-        status.CriDmg = SetStatus(data.baseAtk, myData.statRank[(int)PxmStatus.CriDmg], myData.lv, 0, 10);
-        status.AtkSpd = SetPercentStatus(data.baseAtk, myData.statRank[(int)PxmStatus.Atk], myData.lv, 0, 10);
-        status.Dmg = SetStatus(data.baseAtk, myData.statRank[(int)PxmStatus.AtkDmg], myData.lv, 0, 10);
-        status.SDmg = SetPercentStatus(data.baseAtk, myData.statRank[(int)PxmStatus.SDmg], myData.lv, 0, 10);
-        status.SCri = SetStatus(data.baseAtk, myData.statRank[(int)PxmStatus.SCri], myData.lv, 0, 10);
-        status.SCriDmg = SetPercentStatus(data.baseAtk, myData.statRank[(int)PxmStatus.SCriDmg], myData.lv, 0, 10);
+        
+        status.Atk = SetStatus(data.perAtk, myData.lv, data.lvAtkRate);
+        status.Cri = SetPercentStatus(data.baseCri, myData.lv, 0, 10);
+        status.CriDmg = SetStatus(data.baseCriDmg, myData.lv, 0, 10);
+        status.AtkSpd = SetPercentStatus(data.baseAtkSpd, myData.lv, 0, 10);
+        status.Dmg = SetStatus(data.baseDmg, myData.lv, 0, 10);
+        status.SDmg = SetPercentStatus(data.baseSDmg, myData.lv, 0, 10);
+        status.SCri = SetStatus(data.baseSCri, myData.lv, 0, 10);
+        status.SCriDmg = SetPercentStatus(data.baseSCriDmg, myData.lv, 0, 10);
         return status;
     }
 
-    //일반수치 데이터 수정 필요
-    public static float SetStatus(float baseStat, float statRank, int lv, float statUpLv, float statValue)
+    public static float SetStatus(float perAtk, int lv, float lvAtkRate)
     {
-        return baseStat * statRank / 100 + (1 + (lv - 1) * 0.1f) + (statUpLv * statValue);
+        return perAtk + (lv - 1) * lvAtkRate;
+    }
+
+
+    //일반수치 데이터 수정 필요
+    public static float SetStatus(float baseStat, int lv, float statUpLv, float statValue)
+    {
+        //baseStat* statRank / 100 + (1 + (lv - 1) * 0.1f) + (statUpLv * statValue);
+        return baseStat;
     }
 
     //퍼센트 수치 데이터 수정 필요
-    public static float SetPercentStatus(float baseStat, float statRank, int lv, float statUpLv, float statValue)
+    public static float SetPercentStatus(float baseStat, int lv, float statUpLv, float statValue)
     {
-        return (baseStat * statRank * (1 + ((lv - 1) * 0.1f) + (statUpLv * statValue)) / 100);
+        //(baseStat * statRank * (1 + ((lv - 1) * 0.1f) + (statUpLv * statValue)) / 100);
+        return baseStat;
     }
 
     public static void StatusUp(this PixelmonStatus status, string field, float stat, StatusType type = StatusType.Add)
@@ -34,7 +43,7 @@ public static class PixelmonStatHandler
         {
             var value = (float)fieldInfo.GetValue(status);
             switch(type)
-            { 
+            {
                 case StatusType.Add:
                     fieldInfo.SetValue(status, value + stat);
                     break;
@@ -48,14 +57,36 @@ public static class PixelmonStatHandler
         }
     }
 
-    public static void PxmLvUpgrade(this PixelmonStatus status)
+    public static void PxmLvUpgrade(this PixelmonStatus status, MyPixelmonData myData)
     {
-
+        var saveManager = SaveManager.Instance;
+        saveManager.SetDeltaData("exp", 0);
+        saveManager.SetDeltaData("lv", ++myData.lv);
+        if (myData.lv % 10 == 0)
+        {
+            saveManager.SetDeltaData("maxExp", (int)(myData.maxExp * 1.5f));
+        }
+        else
+        {
+            saveManager.SetDeltaData("maxExp", (int)(myData.maxExp * 1.1f));
+        }
     }
 
-    public static void PxmStarUpgrade(this PixelmonStatus status)
+    public static void PxmStarUpgrade(this PixelmonStatus status, MyPixelmonData myData)
     {
+        if ((myData.star & 1) == 0)
+        {
+            //보유수치 상승
+        }
+        else
+        {
+            //
+        }
 
+        if(myData.star == 5) 
+        { 
+            //패시브 룰렛기능 오픈
+        }
     }
 
     public static void PxmPsvEffect(this PixelmonStatus status)
@@ -65,7 +96,7 @@ public static class PixelmonStatHandler
     
     public static int GetTotalDamage(this PixelmonStatus status, bool isSkill = false)
     {
-        int totalDamage = (int)status.Dmg;
+        int totalDamage = (int)status.Atk;
 
         if (isSkill)
         {
