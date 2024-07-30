8621c2154ebcac46135ba8360551ce999425a0b9
Chloe <144107013+Charen523@users.noreply.github.com>
2024-07-30 10:16:54 +0900

    [feat] UpgradeSlot 생성 및 데이터 부여

---
+++ Assets/02.Scripts/UI/UpgradeTab/UpgradeSlot.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UpgradeSlot : MonoBehaviour
+{
+    public TextMeshProUGUI slotLevelTxt; //Lv.(숫자)
+    public TextMeshProUGUI slotValueTxt; //경우에 따라 % 붙음.
+    public TextMeshProUGUI slotGoldTxt; //끝에 G 붙음.
+
+}
