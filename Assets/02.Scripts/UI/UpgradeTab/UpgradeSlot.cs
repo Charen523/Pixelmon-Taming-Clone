389f64e8def4b1ee9dc8b8dea69d60108a67a3c2
Chloe <144107013+Charen523@users.noreply.github.com>
2024-07-30 11:27:13 +0900

    [feat] 성장탭 레벨데이터 불러오기&저장하기

--- Assets/02.Scripts/UI/UpgradeTab/UpgradeSlot.cs
+++ Assets/02.Scripts/UI/UpgradeTab/UpgradeSlot.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 
 public class UpgradeSlot : MonoBehaviour
 {
+    public UpgradeTab upgradeTab;
+
     public TextMeshProUGUI slotLevelTxt; //Lv.(숫자)
     public TextMeshProUGUI slotValueTxt; //경우에 따라 % 붙음.
     public TextMeshProUGUI slotGoldTxt; //끝에 G 붙음.
 
+
+    public void CalculateMaxPrice()
+    {
+
+    }
 }
--- Assets/02.Scripts/UI/UpgradeTab/UpgradeTab.cs
+++ Assets/02.Scripts/UI/UpgradeTab/UpgradeTab.cs
@@ -6,11 +6,64 @@ public class UpgradeTab : UIBase
 {
     private PixelmonStatus upgradeStat;
 
+    private int currentGold => SaveManager.Instance.userData.gold;
+
     #region UI
     [SerializeField] private UpgradeSlot[] upgradeSlots;
 
-    private int mulValue;
+
     #endregion
+
+    #region Tab Fields & Properties
+
+    private int _mulValue;
+    private int MulValue
+    {
+        get
+        {
+            return _mulValue;
+        }
+        set
+        {
+            if (_mulValue != value)
+            {
+                _mulValue = value;
+            }
+        }
+    }
+
+    public int[] UpgradeLvs
+    {
+        get
+        {
+            return SaveManager.Instance.userData.UpgradeLvs;
+        }
+
+        set
+        {
+            SaveManager.Instance.SetData(nameof(SaveManager.Instance.userData.UpgradeLvs), value);
+        }
+    }
+    public int this[int index]
+    {
+        get
+        {
+            return UpgradeLvs[index];
+        }
+        set
+        {
+            UpgradeLvs[index] = value;
+        }
+    }
+    #endregion
+
+    private void Awake()
+    {
+        for (int i = 0; i < upgradeSlots.Length; i++)
+        {
+            upgradeSlots[i].upgradeTab = this;
+        }
+    }
 
     private void Start()
     {
@@ -20,6 +73,6 @@ public class UpgradeTab : UIBase
 
     public void CurrentToggle(int toggleIndex)
     {
-        mulValue = toggleIndex;
+        MulValue = toggleIndex;
     }
 }
