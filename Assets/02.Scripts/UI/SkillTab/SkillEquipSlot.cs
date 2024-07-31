930824e893de5dedd78e8402451bcd5931e86adb
Euronia12 <lkh0647@naver.com>
2024-08-01 08:26:19 +0900

    [feat] 스킬 장착 슬롯 기능 구현

--- Assets/02.Scripts/UI/SkillTab/SkillEquipSlot.cs
+++ Assets/02.Scripts/UI/SkillTab/SkillEquipSlot.cs
@@ -1,10 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillEquipSlot : MonoBehaviour
 {
-    public MyPixelmonData myPxmData;
-    public MyAtvData myAtvData;
     public SkillTab skillTab;
+    public ActiveData atvData;
+    public MyAtvData myAtvData;
+    public MyPixelmonData myPxmData;
+    public Image equipIcon;
+
+    public void OnClick()
+    {
+        if(skillTab.tabState == TabState.Normal && myAtvData != null)
+            skillTab.OnSkillPopUp(atvData.id);
+        else if(skillTab.tabState == TabState.Equip)
+        {
+            skillTab.allData[skillTab.choiceId].EquipAction();
+            EquipAction(skillTab.allData[skillTab.choiceId]);
+        }
+    }
+
+    public void EquipAction(SkillSlot slot)
+    {
+        atvData = slot.atvData;
+        myAtvData = slot.myAtvData;
+        equipIcon.sprite = atvData.icon;
+        myPxmData.atvSkillId = atvData.id;
+    }
+
+    public void UnEquipAction()
+    {
+        atvData = null;
+        myAtvData = null;
+        equipIcon.sprite = null;
+        myPxmData.atvSkillId = null;
+    }
 }
