72e7eb474130ae05c783de575b85047b1e6c62d9
Euronia12 <lkh0647@naver.com>
2024-08-01 08:27:17 +0900

    [feat]스킬 팝업 기능 구현

--- Assets/02.Scripts/UI/SkillTab/UISkillPopUp.cs
+++ Assets/02.Scripts/UI/SkillTab/UISkillPopUp.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UISkillPopUp : UIBase
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TextMeshProUGUI skillNameTxt;
+    [SerializeField] private TextMeshProUGUI ctTxt;
+    [SerializeField] private TextMeshProUGUI descTxt;
+    [SerializeField] private Button equipBtn;
+    [SerializeField] private TextMeshProUGUI equipTxt;
+    [SerializeField] private SkillSlot copySlot;
+    [SerializeField] private SkillTab skillTab;
+    private string equip = "장착하기";
+    private string unEquip = "해제하기";
+    private int slotIndex;
+    public void ShowPopUp(SkillSlot slot, SkillTab tab)
     {
-        
+        skillTab = tab;    
+        copySlot.InitSlot(tab, slot.atvData);
+        copySlot.myAtvData = slot.myAtvData;
+        copySlot.UpdateSlot();
+
+        slotIndex = slot.atvData.id;
+        skillNameTxt.text = slot.atvData.name;
+        ctTxt.text = slot.atvData.ct.ToString();
+        descTxt.text = slot.atvData.description;
+        equipTxt.text = slot.myAtvData.isEquipped ? unEquip : slot.myAtvData.isOwned ? equip : "-";
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnEquipEvt()
     {
-        
+        skillTab.OnEquip(slotIndex);
     }
 }
