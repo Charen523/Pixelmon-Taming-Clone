9fa82b1e679859e73dd7934144dc17a6be879418
Chloe <144107013+Charen523@users.noreply.github.com>
2024-08-01 11:21:08 +0900

    [feat] Quest 진행을 관리하는 manager

---
+++ Assets/02.Scripts/Manager/QuestManager.cs
@@ -0,0 +1,63 @@
+using TMPro;
+using UnityEngine;
+
+public class QuestManager : Singleton<QuestManager>
+{
+    [SerializeField] private string curQuestId = "R000_Q0";
+    
+    #region UI
+    [SerializeField] private TextMeshProUGUI questNameTxt;
+    [SerializeField] private TextMeshProUGUI countTxt;
+    #endregion
+
+    private QuestData curQData;
+
+    [SerializeField] private int maxQNum = 4;
+
+    private string curQStatus;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        curQuestId = SaveManager.Instance.userData.questId;
+    }
+
+    private void SetQuestNameTxt()
+    {
+
+    }
+
+    private string InsertValues (string description, string goal)
+    {
+        string[] goals = goal.Split ('_');
+
+        foreach (string g in goals)
+        {
+            description = description.Replace("N", g);
+        }
+
+        return description; 
+    }
+
+    private void SetQuestCountTxt()
+    {
+        int status;
+        int goal = 1;
+
+        if (!int.TryParse(curQGoal, out goal))
+        {
+            status = (curQStatus == curQGoal)? 1 : 0;
+        }
+        else
+        {
+            status = int.Parse(curQStatus);
+        }
+
+        countTxt.text = $"({status} / {goal})";
+    }
+
+    private void CurDescription()
+    {
+
+    }
+}
