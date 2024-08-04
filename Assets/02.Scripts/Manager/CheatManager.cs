0d2c1dabe181dabba34c3fdcfff6f92d001f0dde
Chloe <144107013+Charen523@users.noreply.github.com>
2024-08-04 16:04:00 +0900

    [feat] Cheat Manager 추가

---
+++ Assets/02.Scripts/Manager/CheatManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class CheatManager : Singleton<CheatManager>
+{
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            switch(Input.inputString)
+            {
+                case "s":
+                case "S":
+                    NextBossStage();
+                    break;
+                case "g":
+                case "G":
+                    SaveManager.Instance.SetFieldData("gold", 10000, true);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void NextBossStage()
+    {
+        StageManager.Instance.stageNum = 10;
+    }
+}
