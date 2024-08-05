3f2fa2f7c97e7227480c993142dd01c99096a25a
Chloe <144107013+Charen523@users.noreply.github.com>
2024-08-05 18:01:46 +0900

    [feat] 시간에 따른 키 리셋 매커니즘

--- Assets/02.Scripts/UI/DungeonTab/DungeonTab.cs
+++ Assets/02.Scripts/UI/DungeonTab/DungeonTab.cs
@@ -1,24 +1,72 @@
+using System;
 using TMPro;
 using UnityEngine;
 
 public class DungeonTab : UIBase
 {
     SaveManager saveManager;
+    UserData userData;
+
+    private int keyGold => saveManager.userData.keyGold;
+    private int keySeed => saveManager.userData.keySeed;
+    private int keySkill => saveManager.userData.keySkill;
 
     #region UI
-    [SerializeField] private TextMeshProUGUI keyChargeTime;
+  [SerializeField] private TextMeshProUGUI keyChargeTime;
     [SerializeField] private DungeonSlot[] dungeonSlots;
-    private UIBase popup;
+    public UIDungeonEnterPopup popup;
     #endregion
+
+    private bool isInit;
 
     private async void Awake()
     {
+        isInit = false;
         saveManager = SaveManager.Instance;
-        popup = await UIManager.Show<DungeonEnterPopup>();
+        userData = saveManager.userData;
+        popup = await UIManager.Show<UIDungeonEnterPopup>();
+        popup.dungeonTab = this;
+        isInit = true;
+    }
 
-        for (int i = 0; i < dungeonSlots.Length; i++)
+    private void OnEnable()
+    {
+        if (isInit)
         {
-            
+            string lastTime = userData.LastConnectTime;
+            if (DateTime.TryParse(lastTime, out DateTime date))
+            {
+                if (date.Date > DateTime.Now.Date)
+                {
+                    //미래
+                    saveManager.SetFieldData(nameof(userData.keyGold), 3);
+                    saveManager.SetFieldData(nameof(userData.keySeed), 3);
+                    saveManager.SetFieldData(nameof(userData.keySkill), 3);
+                }
+                else if (date.Date < DateTime.Now.Date)
+                {
+                    //과거: 버그임.
+                    Debug.LogError("과거로 오면 안됩니다...앱 강제종료.");
+#if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+                }
+            }
+            saveManager.SetFieldData(nameof(userData.LastConnectTime), DateTime.Now.ToString());
         }
+    }
+
+    private void Update()
+    {//TODO: 최적화 대상. coroutine등으로 초에 1번 부를 것.
+        DateTime now = DateTime.Now;
+        DateTime midnight = now.Date.AddDays(1);
+        TimeSpan timeUntilMidnight = midnight - now;
+
+        keyChargeTime.text = string.Format("{0:D2}:{1:D2}:{2:D2}",
+                                          timeUntilMidnight.Hours,
+                                          timeUntilMidnight.Minutes,
+                                          timeUntilMidnight.Seconds);
     }
 }
