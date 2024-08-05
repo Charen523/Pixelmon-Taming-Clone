7133e176eaf934941a15f17104cdd5ff66245fc9
Chloe <144107013+Charen523@users.noreply.github.com>
2024-08-05 12:36:06 +0900

    [update] 불필요 스크립트 삭제

--- Assets/02.Scripts/UI/Core/CanvasGame.cs
+++ Assets/02.Scripts/UI/Core/CanvasGame.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class CanvasGame : MonoBehaviour
 {
@@ -10,14 +9,9 @@ public class CanvasGame : MonoBehaviour
     IEnumerator Start()
     {
         UILoading.Hide();
-        //yield return SceneManager.LoadSceneAsync("WorldScene", LoadSceneMode.Additive);
         UIManager.SetParents(parents);
         UIManager.SetCanvas(transform);
-        yield return null;
         yield return new WaitUntil(() => DataManager.Instance.isInit);
-        //GameManager.Instance.InitWorld();
-        //yield return null;
-        //yield return UIManager.Show<UIGame>();
         yield return UIManager.Show<UIMiddleBar>();
     }
 }
