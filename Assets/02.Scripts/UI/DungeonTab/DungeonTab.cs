0614514dc6e7b224c349051dbd9eb37362080719
Chloe <144107013+Charen523@users.noreply.github.com>
2024-08-05 12:38:02 +0900

    [feat] 던전탭에서 하위 정보 받아오기

--- Assets/02.Scripts/UI/DungeonTab/DungeonTab.cs
+++ Assets/02.Scripts/UI/DungeonTab/DungeonTab.cs
@@ -1,8 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DungeonTab : UIBase
 {
+    SaveManager saveManager;
 
+    #region UI
+    [SerializeField] private TextMeshProUGUI keyChargeTime;
+    [SerializeField] private DungeonSlot[] dungeonSlots;
+    private UIBase popup;
+    #endregion
+
+    private async void Awake()
+    {
+        saveManager = SaveManager.Instance;
+        popup = await UIManager.Show<DungeonEnterPopup>();
+
+        for (int i = 0; i < dungeonSlots.Length; i++)
+        {
+            
+        }
+    }
 }
