f05734465c66d767892e45950d06d92d0349a5fe
Chloe <144107013+Charen523@users.noreply.github.com>
2024-08-05 18:02:15 +0900

    던전 슬롯에 type 추가

--- Assets/02.Scripts/UI/DungeonTab/DungeonSlot.cs
+++ Assets/02.Scripts/UI/DungeonTab/DungeonSlot.cs
@@ -1,12 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum DungeonType
+{
+    Gold, 
+    Seed,
+    Skill
+}
+
 public class DungeonSlot : MonoBehaviour
 {
-    public Button EnterBtn;
+    public DungeonTab dungeonTab;
+    public DungeonType type;
     public TextMeshProUGUI keyTxt;
 
+    public void enterDungeonBtn()
+    {
+        dungeonTab.popup.SetActive(true);
+        dungeonTab.popup.curDgType = type;
+    }
 }
