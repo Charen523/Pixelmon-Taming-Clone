938e7bd613aa080b940db04b1ed6e59240258371
Chloe <144107013+Charen523@users.noreply.github.com>
2024-08-05 12:37:29 +0900

    [feat] 던전슬롯 스크립트 추가

---
+++ Assets/02.Scripts/UI/DungeonTab/DungeonSlot.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DungeonSlot : MonoBehaviour
+{
+    public Button EnterBtn;
+    public TextMeshProUGUI keyTxt;
+
+}
