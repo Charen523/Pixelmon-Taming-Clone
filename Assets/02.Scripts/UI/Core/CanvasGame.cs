e1bfb6c12e5f798a76bfac79e1f1aee1d22976f9
Chloe <144107013+Charen523@users.noreply.github.com>
2024-08-05 16:33:42 +0900

    [feat] OverLay Btn UIManager로부터 받아오기

--- Assets/02.Scripts/UI/Core/CanvasGame.cs
+++ Assets/02.Scripts/UI/Core/CanvasGame.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CanvasGame : MonoBehaviour
 {
     [SerializeField] private List<Transform> parents;
+    [SerializeField] private Button tabOverlayBtn;
 
     IEnumerator Start()
     {
         UILoading.Hide();
         UIManager.SetParents(parents);
         UIManager.SetCanvas(transform);
+        UIManager.SetTabOverlayBtn(tabOverlayBtn);
         yield return new WaitUntil(() => DataManager.Instance.isInit);
         yield return UIManager.Show<UIMiddleBar>();
     }
