26d0f86f8c962e2512f7034a4efceba93f6c4e7a
SungJiYoon <66246253+ss-zun@users.noreply.github.com>
2024-08-06 17:12:18 +0900

    Feat/skill gacha (#355)
    * [feat] 스킬 뽑기 결과창 구현 완료
    
    * [update] 스킬 뽑기 결과 팝업 프리팹화
    
    * [update] 스킬 뽑기 결과창 연동
    
    * [update] SkillGacha.cs 리팩토링

--- Assets/02.Scripts/UI/ShopTab/UISkillGachaPopup.cs
+++ Assets/02.Scripts/UI/ShopTab/UISkillGachaPopup.cs
@@ -10,20 +10,23 @@ public class UISkillGachaPopup : UIBase
     private const int maxSlotCnt = 30;
 
     private SkillGachaSlot[] slots = new SkillGachaSlot[maxSlotCnt];
-
-    private void Start()
+    private bool isInitSlots;
+    private void InitSlots()
     {
         for (int i = 0; i < maxSlotCnt; i++)
         {
-            slots[i] = new SkillGachaSlot();
             slots[i] = Instantiate(slotPrefab, grid);
             slots[i].gameObject.SetActive(false);
         }
+        isInitSlots = true;
     }
 
     public void SetPopup(int slotCnt, ActiveData[] datas)
     {
-        for(int i = 0;i < slotCnt; i++)
+        if (!isInitSlots)
+            InitSlots();
+
+        for (int i = 0;i < slotCnt; i++)
         {
             slots[i].gameObject.SetActive(true);
             slots[i].InitSlot(datas[i].bgIcon, datas[i].icon);
