e0d318bb1264c15a35efbfc1efbb8e604d132ad4
Euronia12 <lkh0647@naver.com>
2024-08-07 23:08:54 +0900

    [fix] 장착슬롯 클릭 시 0번 스킬 팝업이뜨는 버그 해결

--- Assets/02.Scripts/UI/SkillTab/SkillEquipSlot.cs
+++ Assets/02.Scripts/UI/SkillTab/SkillEquipSlot.cs
@@ -42,6 +42,7 @@ public class SkillEquipSlot : MonoBehaviour
         }
         else if (skillTab.tabState == TabState.Normal && myAtvData.isEquipped)
         {
+            skillTab.choiceId = myAtvData.id;
             skillTab.OnSkillPopUp(skillTab.choiceId);
         }
         else if (skillTab.tabState == TabState.Equip && SaveManager.Instance.userData.equippedPxms[slotIndex].isEquipped)
