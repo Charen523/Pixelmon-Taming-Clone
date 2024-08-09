708c4657d10d6681560ad1ff4ab72abca6e70285
Euronia12 <lkh0647@naver.com>
2024-08-09 21:22:16 +0900

    [update] 수치 업데이트 시 전부 체력회복되는 버그 수정 및 상승치만큼 회복되도록 변경

--- Assets/02.Scripts/Entity/StatHandler/PlayerStatHandler.cs
+++ Assets/02.Scripts/Entity/StatHandler/PlayerStatHandler.cs
@@ -11,11 +11,12 @@ public class PlayerStatHandler : MonoBehaviour
     public float def;
     #endregion
 
-    public void UpdateStats(float perHp, float perDef)
+    public void UpdateStats(float perHp, float perDef, float addHp = 1)
     {
-        maxHp = data.baseMaxHp * (1 + perHp/100);
+        maxHp = data.baseMaxHp * (1 + perHp / 100);
         def = data.baseDef * (1 + perDef / 100);
 
-        Player.Instance.healthSystem.InitHealth(maxHp);
+        Player.Instance.healthSystem.maxHealth = maxHp;
+        Player.Instance.healthSystem.currentHealth += data.baseMaxHp * addHp;
     }
 }
