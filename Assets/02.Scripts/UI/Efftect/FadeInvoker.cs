cf9f9bab56896e2120ebe65e4937eee99fafe925
Euronia12 <lkh0647@naver.com>
2024-08-11 20:28:21 +0900

    [feat] 핫픽스 페이드 인 시 캐릭터 위치 초기화

--- Assets/02.Scripts/UI/Efftect/FadeInvoker.cs
+++ Assets/02.Scripts/UI/Efftect/FadeInvoker.cs
@@ -21,8 +21,8 @@ public class FadeInvoker : MonoBehaviour
             isUsing = true;
             image.color = new Color(0, 0, 0, 0);
             yield return waitFadeTime;
-            image.DOFade(1f, 0.5f);
-            yield return waitFadeTime;
+            image.DOFade(1f, 0.5f).OnComplete(() => Player.Instance.gameObject.transform.position = Vector3.zero);
+            yield return waitFadeTime;    
             isUsing = false;
         }
     }
