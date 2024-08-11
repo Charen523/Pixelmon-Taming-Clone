9bd53cbe550d1f6a8423706195eb1d42cb97b2c0
Chloe <144107013+Charen523@users.noreply.github.com>
2024-08-12 00:22:04 +0900

    [feat] 스테이지마다 몬스터 강해지기: 임시

--- Assets/02.Scripts/Entity/StatHandler/EnemyStatHandler.cs
+++ Assets/02.Scripts/Entity/StatHandler/EnemyStatHandler.cs
@@ -16,14 +16,18 @@ public class EnemyStatHandler : MonoBehaviour
 
     public void UpdateEnemyStats()
     {
-        int difficulty = 0;
-        int deltaDiff = (int)Mathf.Pow(2, difficulty);
+        int difficulty = StageManager.Instance.diffNum;
+        int deltaDiff = difficulty + 1;
 
-        enemyAtk = data.atk * deltaDiff;
-        enemyMaxHp = data.hp * deltaDiff;
-        enemyDef = data.def * deltaDiff;
-        enemyCri = data.cri * deltaDiff;
-        enemyCriDmg = data.criDmg * deltaDiff;
+        int world = StageManager.Instance.worldNum;
+        int stage = StageManager.Instance.stageNum;
+
+        float deltaStage = (world - 1) * 20 + stage;
+
+
+        enemyAtk = data.atk * (deltaStage / 100) * deltaDiff;
+        enemyMaxHp = data.hp * (deltaStage / 100) * deltaDiff;
+        enemyDef = data.def * (deltaStage / 100) * deltaDiff;
 
         enemy.healthSystem.initEnemyHealth(enemyMaxHp);
     }
