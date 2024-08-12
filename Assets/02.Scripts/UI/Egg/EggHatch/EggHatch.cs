0cbbf04039707e89ec4cbd34589db8b728a696f1
Euronia12 <lkh0647@naver.com>
2024-08-12 22:09:40 +0900

    [fix]상속 클래스 수정

--- Assets/02.Scripts/UI/Egg/EggHatch/EggHatch.cs
+++ Assets/02.Scripts/UI/Egg/EggHatch/EggHatch.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EggHatch : Singleton<EggHatch>
+public class EggHatch : MonoBehaviour
 {
     #region 애니메이션
     public AnimationData AnimData = new AnimationData();
