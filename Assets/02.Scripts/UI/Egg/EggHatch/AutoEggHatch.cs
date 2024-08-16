8cecc5399b99fd70512b841429aac36701d5138c
SungJiYoon <66246253+ss-zun@users.noreply.github.com>
2024-08-16 21:03:16 +0900

    [update] 로고 새로운 문구 업데이트 (#538)

---
+++ Assets/02.Scripts/UI/Egg/EggHatch/AutoEggHatch.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoEggHatch : UIBase
+{
+    public override void Opened(object[] param)
+    {
+        // UI 셋팅
+    }
+
+
+}
