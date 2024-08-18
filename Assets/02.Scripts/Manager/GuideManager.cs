5b3902a31996cf04f3a24552b969a409e78527cc
Chloe <144107013+Charen523@users.noreply.github.com>
2024-08-18 22:41:00 +0900

    [feat] Guide를 위한 이벤트 & 검색을 위한 HashSet 추가

--- Assets/02.Scripts/Manager/GuideManager.cs
+++ Assets/02.Scripts/Manager/GuideManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public enum OpenTabType
@@ -13,12 +15,18 @@ public class GuideManager : Singleton<GuideManager>
     public GameObject GuideArrow;
     public GameObject PxmToggle;
     public int guideNum = 0;
+    public int guideArrowIndex = 0;
     private UserData userData => SaveManager.Instance.userData;
 
     #region Tutorial Start Indexes
+    private readonly HashSet<int> guideNumSet = new HashSet<int> { 3, 6, 9, 10, 21, 23, 35, 45, 51 };
+    public event Action<int> OnGuideAction;
+    public event Action<int> OnArrowAction;
+
     public readonly int equipPixelmon = 1;
     public readonly int setAllPixelmon = 3;
     public readonly int nestLvUp = 6;
+    public readonly int upgrAtk = 9;
     public readonly int feedPixelmon = 10;
     public readonly int skillGatcha = 21;
     public readonly int skillEquip = 23;
@@ -40,10 +48,36 @@ public class GuideManager : Singleton<GuideManager>
         base.Awake();        
     }
 
+    public void SetArrow(GameObject obj, float addYPos = 0)
+    {
+        Vector3 currentPosition = obj.transform.position;
+        Vector3 newPosition = new Vector3(currentPosition.x, currentPosition.y + 20f + addYPos, currentPosition.z);
+        GuideArrow.transform.position = newPosition;
+    }
+
+    public void GuideNumTrigger(int guideNum)
+    {
+        if (guideNumSet.Contains(guideNum))
+        {
+            OnGuideAction?.Invoke(guideNum);
+        }
+    }
+
+    public void GuideArrowNumTrigger(int arrowNum)
+    {
+        OnArrowAction?.Invoke(arrowNum);
+    }
+
     public void SetBottomLock()
     {
         Locks[0].SetActive(false);
 
+        if (guideNum == 1 && SaveManager.Instance.userData.isSetArrowOnEgg)
+        {
+            GuideArrow.SetActive(false);
+            return;
+        }
+        
         if (guideNum > tabUpgr)
         {
             Locks[1].SetActive(false);
@@ -64,15 +98,5 @@ public class GuideManager : Singleton<GuideManager>
         {
             Locks[4].SetActive(false);
         }
-        
-        if (SaveManager.Instance.userData.isSetArrowOnEgg)
-            GuideArrow.SetActive(false);
-    }
-
-    public void SetArrow(GameObject obj, float addYPos = 0)
-    {
-        Vector3 currentPosition = obj.transform.position;
-        Vector3 newPosition = new Vector3(currentPosition.x, currentPosition.y + 20f + addYPos, currentPosition.z);
-        GuideArrow.transform.position = newPosition;
     }
 }
