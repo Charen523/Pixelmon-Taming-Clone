ccbf4ddba1f1e84f54d8f0ec4ad3f489a8b24f62
Chloe <144107013+Charen523@users.noreply.github.com>
2024-08-18 22:39:13 +0900

    [feat] BottomBar부터 시작하는 Guide 추가

--- Assets/02.Scripts/UI/Core/UIBottomBar.cs
+++ Assets/02.Scripts/UI/Core/UIBottomBar.cs
@@ -15,9 +15,14 @@ enum eTabs
 
 public class UIBottomBar : MonoBehaviour
 {
+    GuideManager guideManager;
+
     private List<UIBase> uiTabs = new List<UIBase>();
     [SerializeField] private GameObject overlayPanel;
     [SerializeField] private List<Toggle> toggles;
+
+    private bool isGuideOn = false;
+    private int guidingToggle = -1;
 
     int selectedIndex { get => toggles.FindIndex(obj  => obj.isOn); }
 
@@ -29,6 +34,9 @@ public class UIBottomBar : MonoBehaviour
         {
             uiTabs.Add(await UIManager.Show(names[i]));
         }
+
+        guideManager = GuideManager.Instance;
+        guideManager.OnGuideAction += SetGuideArrow;
     }
 
     public void OnvalueChanged()
@@ -37,7 +45,7 @@ public class UIBottomBar : MonoBehaviour
         
         if (int.TryParse(nextObject.name, out int index))
         {
-            if (GuideManager.Instance.Locks[index].activeInHierarchy)
+            if (guideManager.Locks[index].activeInHierarchy)
             {
                 string msg = "";
                 switch (index)
@@ -68,16 +76,81 @@ public class UIBottomBar : MonoBehaviour
                         break;
                 }
             }
+            else if (isGuideOn && index == guidingToggle)
+            {
+                guideManager.GuideArrow.SetActive(false);
+                guideManager.guideArrowIndex++;
+            }
         }
 
         if (!SaveManager.Instance.userData.isDoneTutorial)
-            GuideManager.Instance.SetArrow(GuideManager.Instance.PxmToggle.gameObject);
-
+        {
+            guideManager.SetArrow(guideManager.PxmToggle.gameObject);
+        }
+        
         overlayPanel.SetActive(selectedIndex >= 0);
         uiTabs.ForEach(obj => obj.gameObject.SetActive(false)); //모든 탭 끄기.
         if (selectedIndex >= 0)
         {
             uiTabs[selectedIndex].gameObject.SetActive(true);   
+            if (isGuideOn && guidingToggle == selectedIndex)
+            {
+                isGuideOn = false;
+                guidingToggle = -1;
+
+                switch(selectedIndex)
+                {
+                    case 0:
+                        PixelmonTab tab = uiTabs[selectedIndex].GetComponent<PixelmonTab>();
+                        tab.InvokePixelmonTabGuide();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    public void SetGuideArrow(int guideIndex)
+    {
+        guideManager.GuideArrow.SetActive(true);
+        guideManager.guideArrowIndex = 1;
+        isGuideOn = true;
+
+        switch (guideIndex)
+        {    
+            case 3: //일괄편성
+                guideManager.SetArrow(toggles[0].gameObject);
+                guidingToggle = 0;
+                break;
+            case 9: //공격력 업그레이드
+                guideManager.SetArrow(toggles[1].gameObject);
+                guidingToggle = 1;
+                break;
+            case 10: //먹이주기
+                guideManager.SetArrow(toggles[0].gameObject);
+                guidingToggle = 0;
+                break;
+            case 21: //스킬 뽑기
+                guideManager.SetArrow(toggles[5].gameObject);
+                guidingToggle = 5;
+                break;
+            case 23: //스킬 장착
+                guideManager.SetArrow(toggles[2].gameObject);
+                guidingToggle = 2;
+                break;
+            case 35: //씨앗심기
+                guideManager.SetArrow(toggles[3].gameObject);
+                guidingToggle = 3;
+                break;
+            case 45: //수확하기
+                guideManager.SetArrow(toggles[3].gameObject);
+                guidingToggle = 3;
+                break;
+            case 51: //골드 던전
+                guideManager.SetArrow(toggles[4].gameObject);
+                guidingToggle = 4;
+                break;
         }
     }
 }
