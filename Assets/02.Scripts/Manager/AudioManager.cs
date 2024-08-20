0e2ff665b65ed15b38e57bb02d92793de26202cf
Euronia12 <lkh0647@naver.com>
2024-08-20 21:09:32 +0900

    [update] 오디오 매니저 수정
    배경음 전환 구현

--- Assets/02.Scripts/Manager/AudioManager.cs
+++ Assets/02.Scripts/Manager/AudioManager.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Audio;
 
@@ -13,7 +14,8 @@ public class AudioManager : Singleton<AudioManager>
     [SerializeField] private AudioMixerGroup bgmMixerGroup;
     [SerializeField] private AudioMixerGroup sfxMixerGroup;
 
-    [SerializeField] private AudioSource bgmAudioSource;
+    [SerializeField] private bool isFirstAudio;
+    [SerializeField] private AudioSource[] bgmAudioSource;
     [SerializeField] private AudioClip[] bgmClip;
 
     public bool[] isMuted = new bool[3];
@@ -26,12 +28,29 @@ public class AudioManager : Singleton<AudioManager>
 
     public void ChangeBackGroundMusic(BgmIndex clip)
     {
-        AudioClip music = bgmClip[(int)clip];
-        bgmAudioSource.Stop();
-        bgmAudioSource.clip = music;
-        bgmAudioSource.Play();
+        if (bgmAudioSource[0].isPlaying)
+        {
+            SetBgAudio(clip, 0, 1);
+        }
+        else if (bgmAudioSource[1].isPlaying)
+        {
+            SetBgAudio(clip, 1, 0);
+        }
+        else
+        {
+            bgmAudioSource[0].DOComplete();
+            bgmAudioSource[0].volume = 0;
+            SetBgAudio(clip, 1, 0);
+        }
     }
 
+    private void SetBgAudio(BgmIndex clip, int stopIndex, int playIndex)
+    {
+        bgmAudioSource[playIndex].clip = bgmClip[(int)clip];
+        bgmAudioSource[playIndex].Play();
+        bgmAudioSource[playIndex].DOFade(0.5f, 2);
+        bgmAudioSource[stopIndex].DOFade(0, 2).OnComplete(() => bgmAudioSource[stopIndex].Stop());
+    }
     public static void PlayClip(AudioClip clip)
     {
         //효과음?
